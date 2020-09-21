38f8e24c4b52b9688e9895d0233f6bf53c862db7
Ken Tauchi <kentauchica@gmail.com>
2020-09-20 19:47:12 -0700

    Updates

---
+++ precache-manifest.1239676340b5f0f6ea9782e1992249fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af774948fc99593368d9565730c1807",
+    "url": "/covid-usa-chart-react/index.html"
+  },
+  {
+    "revision": "5254a7d8047f2a57c8cd",
+    "url": "/covid-usa-chart-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f8819b12aa7f232aee0",
+    "url": "/covid-usa-chart-react/static/js/2.91780818.chunk.js"
+  },
+  {
+    "revision": "61fb2e8d130dede9cb27bd885eb0f4f0",
+    "url": "/covid-usa-chart-react/static/js/2.91780818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5254a7d8047f2a57c8cd",
+    "url": "/covid-usa-chart-react/static/js/main.1a4e391f.chunk.js"
+  },
+  {
+    "revision": "27ee946d808b9820f795",
+    "url": "/covid-usa-chart-react/static/js/runtime-main.e3f0c1f2.js"
+  }
+]);
